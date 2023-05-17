b08d32c06904f2ffcbf6078a6db60dfdb164b3f9
buziusbastus1 <kubaotreba121@wp.pl>
2023-05-18 00:39:29 +0200

    modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -13,7 +13,7 @@ interface ModalProps {
   actionLabel: string;
   disabled?: boolean;
   secondaryAction?: () => void;
-  secondaryLabel?: string;
+  secondaryActionLabel?: string;
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -26,7 +26,7 @@ const Modal: React.FC<ModalProps> = ({
   actionLabel,
   disabled,
   secondaryAction,
-  secondaryLabel,
+  secondaryActionLabel,
 }) => {
   const [showModal, setShowModal] = useState(isOpen);
   useEffect(() => {
@@ -85,7 +85,19 @@ ${showModal ? "opacity-100" : "opacity-0"}
                 {/* footer */}
                 <div className="flex flex-col gap-2 p-6">
                   <div className="flex flex-row items-center gap-4 w-full">
-                    <Button label="butt" />
+                    {secondaryAction && secondaryActionLabel && (
+                      <Button
+                        outline
+                        disabled={disabled}
+                        label={secondaryActionLabel}
+                        onClick={handleSecondaryAction}
+                      />
+                    )}
+                    <Button
+                      disabled={disabled}
+                      label={actionLabel}
+                      onClick={handleSubmit}
+                    />
                   </div>
                 </div>
               </div>
---
+++ app/hooks/useRegisterModal.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+interface RegisterModalStore{
+isOpen:boolean
+onOpen:()=>void
+onClose:()=>void
+}
+
+const useRegisterModal=create<RegisterModalStore>((set)=>({
+    isOpen:false,
+    onOpen:()=>({isOpen:true}),
+    onClose:()=>({isOpen:false})
+}))
+export default useRegisterModal
