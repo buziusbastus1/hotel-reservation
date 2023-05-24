3f2b5251cd8df4783d2b7b29d445a15e1c62f530
buziusbastus1 <kubaotreba121@wp.pl>
2023-05-24 23:49:48 +0200

    modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -26,16 +26,17 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         <div
           onClick={() => {}}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-        ></div>
-        home
-      </div>
-      <div
-        onClick={toggleOpen}
-        className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-      >
-        <AiOutlineMenu />
-        <div className="hidden md:block">
-          <Avatar src={currentUser?.image} />
+        >
+          home
+        </div>
+        <div
+          onClick={toggleOpen}
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar src={currentUser?.image} />
+          </div>
         </div>
       </div>
       {isOpen && (
