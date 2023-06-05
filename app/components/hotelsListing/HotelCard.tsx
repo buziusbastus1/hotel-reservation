19aea23a5cdca4a72edfa9b5c9c28068deae78d1
buziusbastus1 <kubaotreba121@wp.pl>
2023-06-05 23:37:54 +0200

    api

--- app/components/hotelsListing/HotelCard.tsx
+++ app/components/hotelsListing/HotelCard.tsx
@@ -1,11 +1,19 @@
 "use client";
-import { SafeUser } from "@/app/types";
-import { Listing, Reservation } from "@prisma/client";
-import { useRouter } from "next/navigation";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeListing, SafeUser } from "@/app/types";
+
 import HeartButton from "../HeartButton";
-interface HotelCardProps {
-  data: Listing;
+import Button from "../Button";
+import ClientOnly from "../ClientOnly";
+import { format } from "date-fns";
+
+interface ListingCardProps {
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
@@ -13,31 +21,79 @@ interface HotelCardProps {
   actionId?: string;
   currentUser?: SafeUser | null;
 }
-const HotelCard: React.FC<HotelCardProps> = ({
+
+const ListingCard: React.FC<ListingCardProps> = ({
   data,
   reservation,
   onAction,
   disabled,
   actionLabel,
-  actionId,
+  actionId = "",
   currentUser,
 }) => {
   const router = useRouter();
-  // const { getByValue } = useCountries();
+  const { getByValue } = useCountries();
+
+  const location = getByValue(data.locationValue);
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (disabled) {
+        return;
+      }
+
+      onAction?.(actionId);
+    },
+    [disabled, onAction, actionId]
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return data.price;
+  }, [reservation, data.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
   return (
     <div
       onClick={() => router.push(`/listings/${data.id}`)}
-      className="group col-span-1 cursor-pointer"
+      className="col-span-1 cursor-pointer group"
     >
       <div className="flex flex-col gap-2 w-full">
-        {/* <img src={data.imageSrc} alt="h" /> */}
-        <div className="apect-square w-full relative overflow-hidden rounded-xl">
-          aha
+        <div
+          className="
+            aspect-square 
+            w-full 
+            relative 
+            overflow-hidden 
+            rounded-xl
+          "
+        >
           <Image
             fill
-            alt="listing"
+            className="
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+            "
             src={data.imageSrc}
-            className="object-cover h-full w-full group-hover:scale-110 transition"
+            alt="Listing"
           />
           <div
             className="
@@ -49,9 +105,27 @@ const HotelCard: React.FC<HotelCardProps> = ({
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
 };
 
-export default HotelCard;
+export default ListingCard;
