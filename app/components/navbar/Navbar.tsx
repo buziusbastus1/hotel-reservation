ebbd310b83543e88b3e8057b80bcdfa06c9c8a38
buziusbastus1 <kubaotreba121@wp.pl>
2023-06-13 12:09:16 +0200

    whie

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,19 +1,16 @@
-"use client";
-
 import Container from "../Container";
 import Categories from "./Categories";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
-
 interface NavbarProps {
   currentUser?: SafeUser | null;
 }
-const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
-  // console.log({ currentUser });
 
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+  console.log({ currentUser });
   return (
-    <div className="fixed w-full bg-whie z-10 shadow-sm">
+    <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
