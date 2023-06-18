25ced2e5a7b468785a9f3ddcadea103a032c2d4f
buziusbastus1 <kubaotreba121@wp.pl>
2023-06-19 00:59:31 +0200

    card

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import EmmptyState from "./components/EmptyState";
 import HotelCard from "./components/hotelsListing/HotelCard";
 import Categories from "./components/hotelsListing/Categories";
 import Slider from "./components/Slider";
-// import { Carousel } from "@material-tailwind/react";
 
 export default async function Home() {
   const listings = await getListings();
@@ -23,11 +22,7 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <Categories />
-        {/* <div className="
-          pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"> */}
-
         <Slider>
-          {/* <div className="flex justify-center"> */}
           {listings.map((listing) => {
             return (
               <HotelCard
@@ -37,11 +32,22 @@ export default async function Home() {
               />
             );
           })}
-          {/* </div> */}
         </Slider>
-        {/* </div> */}
+        <Categories />
+        <Slider>
+          {listings.map((listing) => {
+            return (
+              <HotelCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
+        </Slider>
       </Container>
     </ClientOnly>
   );
 }
+
 // <div>{listing.title}</div>
