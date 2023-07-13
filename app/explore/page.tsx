8bb0185083be58d0e9f285ff15ecbd7af005c157
buziusbastus1 <kubaotreba121@wp.pl>
2023-07-13 15:36:02 +0200

    deploy

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,24 +1,11 @@
 import getCurrentUser from "../actions/getCurrentUser";
-import getFavoriteListings from "../actions/getFavoriteListings";
-import getListings, { IListingsParams } from "../actions/getListings";
-import EmptyState from "../components/EmptyState";
-import { categories } from "../components/hotelsListing/Categories";
-import { SafeListing } from "../types";
+import getListings from "../actions/getListings";
 import ExploreClient from "./ExploreClient";
 
 const TripsPage = async () => {
-  const searchParams: IListingsParams = {};
-  const listings = await getListings(searchParams);
-
+  const listings = await getListings();
   const currentUser = await getCurrentUser();
-  return (
-    <ExploreClient
-      listings={listings}
-      currentUser={currentUser}
-      // category={categories}
-      // icon={category.icon}
-      // label={category?.label}
-    />
-  );
+
+  return <ExploreClient listings={listings} currentUser={currentUser} />;
 };
 export default TripsPage;
